ab46bdf331912439977c139a5a957908ce58c9dd
niklas-hjelm <niklas.hjelm@iths.se>
2023-12-08 09:00:31 +0100

    update

--- LINQDemo/Data/Person.cs
+++ LINQDemo/Data/Person.cs
@@ -5,5 +5,5 @@ public record Person
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public int Age { get; set; }
-    public List<Product> Cart { get; } = new();
+    public List<Product> Cart { get; } = new List<Product>();
 }
